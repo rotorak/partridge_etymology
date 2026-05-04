9e6c48cd507c7a415ffbcfe2b644f924dbc9f8ef
rotorak <trob777@hotmail.com>
2026-05-04 04:25:25 -0400

    cos serviceworker and deployment.yml

--- origins/vite.config.ts
+++ origins/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/partridge_etymology/',
   assetsInclude: ['**/*.db'],
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
